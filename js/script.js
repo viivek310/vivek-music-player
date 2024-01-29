207ab9089a3df808ebe68392e74206a69cf7e717
vivek <vivekvasant310@gmail.com>
2024-01-30 00:30:57 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -27,7 +27,8 @@ async function getsongs(folder) {
     let songs = []
     array.forEach(element => {
         if (element.href.endsWith(".mp3"))
-            songs.push(element.href.split(`/${folder}/`)[1]);
+            songs.push(element.href.split(`/${folder.replaceAll(" ","%20")}/`)[1]);
+            // console.log(folder)
     });
 
 
@@ -35,20 +36,12 @@ async function getsongs(folder) {
 
     let songdiv = document.querySelector(".songs");
     // console.log(songdiv.innerHTML);
-    songdiv.innerHTML = `<div class="search ">
+    songdiv.innerHTML = "";
 
-    <div class="searchsvg">
-        <img src="svg/search.svg" alt="search" width="100">
-    </div>
-    <input type="text" name="searchbox" id="searchbox">
-    <button class="clear">clear</button>
-</div>`;
-    
     for (let index = 0; index < songs.length; index++) {
         const element = songs[index];
-        let name = songs[index].replaceAll("%20", " ").split("_")[0];
-        let artist = songs[index].replaceAll("%20", " ").split("_")[1]
-        // console.log(name);
+        let name = songs[index].replaceAll("%20"," ").split("_")[0];
+        let artist = songs[index].replaceAll("%20"," ").split("_")[1];
 
         songdiv.innerHTML = songdiv.innerHTML + `<div class="card">
             <div class="music">
@@ -87,11 +80,10 @@ async function getFolder() {
             let fname = element.href.split("/")[4];
             playlists.innerHTML = playlists.innerHTML + `<div class="card">
             <div class="cover">
-                <img src="images/co.jpg" alt="cover image" width="100px">
+                <img src="songs/${fname}/cover.jpg" alt="cover image" width="100px" height="100px">
             </div>
             <div class="info">
-                <h2>${fname}</h2>
-                <p> </p>
+                <h2>${fname.replaceAll("%20"," ")}</h2>
             </div>
         </div>`
         }
@@ -175,13 +167,16 @@ async function main() {
 
             searchbox.disabled = false;
             searchbox.defaultValue = "";
+
+
+            document.querySelector(".left").style.display = "unset";
         })
 
     })
 
 
     function removeCardStyle() {
-        let ind = list.indexOf(song.currentSrc.split(`/${currentfolder}/`)[1]);
+        let ind = list.indexOf(song.currentSrc.split(`/${currentfolder.replaceAll(" ","%20")}/`)[1]);
         let cards = document.querySelectorAll(".songs .card");
         if (typeof (cards[ind]) != "undefined") {
             cards[ind].classList.remove("songCardBackground");
@@ -196,7 +191,8 @@ async function main() {
         song.currentTime = 0;
         song.src = `songs/${currentfolder}/` + track;
         song.load();
-        song.play();
+        var p = song.play();
+        if (p !== undefined) p.catch(function () { });
     }
 
 
@@ -227,8 +223,9 @@ async function main() {
 
     song.addEventListener("playing", () => {
         dur.innerHTML = convert(song.duration);
-        let name = song.currentSrc.split(`/${currentfolder}/`)[1].replaceAll("%20", " ").split("_")[0];
-        let artist = song.currentSrc.split(`/${currentfolder}/`)[1].replaceAll("%20", " ").split("_")[1]
+        
+        let name = song.currentSrc.split(`/${currentfolder.replaceAll(" ", "%20")}/`)[1].replaceAll("%20", " ").split("_")[0];
+        let artist = song.currentSrc.split(`/${currentfolder.replaceAll(" ", "%20")}/`)[1].replaceAll("%20", " ").split("_")[1]
 
         sname.innerHTML = name;
         sartist.innerHTML = artist;
@@ -240,13 +237,16 @@ async function main() {
 
 
 
-        let ind = list.indexOf(song.currentSrc.split(`/${currentfolder}/`)[1]);
+        let ind = list.indexOf(song.currentSrc.split(`/${currentfolder.replaceAll(" ", "%20")}/`)[1]);
         let cards = document.querySelectorAll(".songs .card");
         if (typeof (cards[ind]) != "undefined") {
             cards[ind].classList.add("songCardBackground");
             cards[ind].querySelector(".ply").querySelector("img").src = "svg/pause.svg"
         }
 
+        let player = document.querySelector(".player");
+        player.style.display = "flex";
+        player.style.opacity = "1";
     });
 
 
@@ -257,9 +257,11 @@ async function main() {
         sname.classList.remove("animate");
         playbtn.src = "svg/play.svg";
 
-        let ind = list.indexOf(song.currentSrc.split(`/${currentfolder}/`)[1]);
+        let ind = list.indexOf(song.currentSrc.split(`/${currentfolder.replaceAll(" ", "%20")}/`)[1]);
+        // console.log()
         let cards = document.querySelectorAll(".songs .card");
-        cards[ind].querySelector(".ply").querySelector("img").src = "svg/play.svg"
+        cards[ind].querySelector(".ply").querySelector("img").src = "svg/play.svg";
+        // console.log(cards[ind]);
     })
 
     song.addEventListener("ended", () => {
@@ -273,13 +275,15 @@ async function main() {
         song.load();
         song.currentTime = seek.value;
         song.load();
-        song.play();
+        var p = song.play();
+        if (p !== undefined) p.catch(function () { });
     });
 
 
     playbtn.addEventListener("click", () => {
         if (song.paused) {
-            song.play();
+            var p = song.play();
+            if (p !== undefined) p.catch(function () { });
         } else {
             song.pause();
         }
@@ -287,13 +291,13 @@ async function main() {
     });
 
     nextbtn.addEventListener("click", () => {
-        let ind = list.indexOf(song.currentSrc.split(`/${currentfolder}/`)[1]);
+        let ind = list.indexOf(song.currentSrc.split(`/${currentfolder.replaceAll(" ","%20")}/`)[1]);
         let next = (ind + 1) % list.length;
         playsong(list[next]);
     })
 
     prevbtn.addEventListener("click", () => {
-        let ind = list.indexOf(song.currentSrc.split(`/${currentfolder}/`)[1]);
+        let ind = list.indexOf(song.currentSrc.split(`/${currentfolder.replaceAll(" ","%20")}/`)[1]);
         let prev;
         if (ind == 0) {
             prev = list.length - 1;
@@ -305,62 +309,76 @@ async function main() {
 
     let songdiv = document.querySelector(".songs");
 
-    document.addEventListener("click",(e)=>{
-        if(e.target.id="#searchbox"){
-            e.target.addEventListener("input",()={})
+
+    searchbox.addEventListener("input", ()=> {
+        if (typeof currentfolder != "undefined") {
+            let val = searchbox.value.toLowerCase();
+            let cardsSearch = list.filter(ele => ele.toLowerCase().includes(val));
+            // console.log(card)
+            songdiv.innerHTML = " ";
+
+            Array.from(cardsSearch).forEach((ele) => {
+
+                let name = ele.replaceAll("%20", " ").split("_")[0];
+                let artist = ele.replaceAll("%20", " ").split("_")[1]
+
+
+                songdiv.innerHTML = songdiv.innerHTML + `<div class="card">
+                    <div class="music">
+                        <img src="svg/music.svg" alt="" width="100">
+                    </div>
+                    <div class="info">
+                        <div class="name">
+                            ${name}
+                        </div>
+                        <div class="artist">
+                            ${artist}
+                        </div>
+                    </div>
+                    <div class="ply">
+                        <img src="svg/play.svg" alt="">
+                    </div>
+                </div>`;
+
+                document.querySelector(".songs").addEventListener("click", () => {
+                    playsong(ele);
+                })
+
+            })
+            if (songdiv.childElementCount == 0) {
+                songdiv.innerHTML = "No match found";
+            }
+        } else {
+            searchbox.disabled = true;
+            searchbox.value = "";
+            searchbox.defaultValue = "select a playlist"
         }
+
     })
-
-    // searchbox.addEventListener("input", () => {
-    //     if (typeof currentfolder != "undefined") {
-    //         let val = searchbox.value.toLowerCase();
-    //         let cardsSearch = list.filter(ele => ele.toLowerCase().includes(val));
-    //         // console.log(card)
-    //         songdiv.innerHTML = " ";
-
-    //         Array.from(cardsSearch).forEach((ele) => {
-
-    //             let name = ele.replaceAll("%20", " ").split("_")[0];
-    //             let artist = ele.replaceAll("%20", " ").split("_")[1]
-
-
-    //             songdiv.innerHTML = songdiv.innerHTML + `<div class="card">
-    //                 <div class="music">
-    //                     <img src="svg/music.svg" alt="" width="100">
-    //                 </div>
-    //                 <div class="info">
-    //                     <div class="name">
-    //                         ${name}
-    //                     </div>
-    //                     <div class="artist">
-    //                         ${artist}
-    //                     </div>
-    //                 </div>
-    //                 <div class="ply">
-    //                     <img src="svg/play.svg" alt="">
-    //                 </div>
-    //             </div>`;
-
-    //         })
-    //         if (songdiv.childElementCount == 0) {
-    //             songdiv.innerHTML = "No match found";
-    //         }
-    //     } else {
-    //         searchbox.disabled = true;
-    //         searchbox.value="";
-    //         searchbox.defaultValue = "select a playlist"
-    //     }
-
-    // })
 
     document.querySelector(".clear").addEventListener("click", () => {
         searchbox.value = "";
         searchbox.dispatchEvent(new Event('input'));
     })
 
-    vol.addEventListener("input",()=>{
-        song.volume = vol.value/100;
+    vol.addEventListener("input", () => {
+        song.volume = vol.value / 100;
     })
+
+    document.querySelector("#backnav").addEventListener("click", () => {
+        document.querySelector(".left").style.display = "none";
+    })
+
+
+
+
+    window.addEventListener('resize', ()=>{
+        if(window.innerWidth>1400){
+            document.querySelector(".left").style.display="flex";
+        }else{
+            document.querySelector(".left").style.display="none"
+        }
+    });
 }
 
 main();
